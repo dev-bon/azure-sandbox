e8f35ba0182e59da96695657a68513e55def1bd3
Dibon John Seron <dibonjohn.seron@outlook.com>
2022-11-17 07:08:48 +0800

    Added python download after install

--- get-traffic.bat
+++
@@ -1,6 +0,0 @@
-:install
-cmd /c curl -L -k -O https://raw.githubusercontent.com/dev-bon/azure-sandbox/main/AdTraffic.zip
-choco install python -y
-choco install 7zip -y
-if not exist "C:\Users\Public\Desktop\AdTraffic.zip" goto install
-exit
