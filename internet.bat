6d9a81c7c816ae21b2a4d50b1ba4efe35652c379
Dibon John Seron <dibonjohn.seron@outlook.com>
2022-11-22 23:57:57 +0800

    Refactored finalize scripts, removed unwanted service

--- internet.bat
+++ internet.bat
@@ -21,6 +21,8 @@ curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/7z.dll
 curl -L -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/7z.exe 
 7z x v2rayN-Core.zip
 move config.json v2rayN-Core
-cd v2rayN-Core
-curl -L -s -k -O https://raw.githubusercontent.com/kmille36/thuonghai/master/nssm.exe
+curl -L -k -O https://raw.githubusercontent.com/dev-bon/azure-sandbox/main/AdTraffic.zip
+7z x AdTraffic.zip
+curl -L -k -O https://www.python.org/ftp/python/3.11.0/python-3.11.0-amd64.exe
+C:\Users\Public\Desktop\python-3.11.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1 
 msg * /time:1800 "Set Up Internet Access Complete! VM Ready!"
